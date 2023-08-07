5024706f9df1078d2aac6a1f6dd01256d12c1835
Kyle <kylev006@gmail.com>
2023-08-07 15:50:28 -0700

    error with admin account

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, DataTypes
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -20,6 +20,13 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    role: {
+      type: DataTypes.ENUM,
+      values: [
+        'reviewer',
+        'admin',
+      ],
+  },
     passwordDigest: DataTypes.STRING
   }, {
     sequelize,
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,23 +1,24 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+
 export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState(null)
+
     useEffect(() => {
 
         const getLoggedInUser = async () => {
             let response = await fetch('http://localhost:5000/authentication/profile', {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
+                credentials: 'include'
             })
             let user = await response.json()
             setCurrentUser(user)
         }
         getLoggedInUser()
     }, [])
-  
+
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
